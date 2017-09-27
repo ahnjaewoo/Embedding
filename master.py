971ca120569c1db5bd766451be67d12eaa67a5eb
강경필 <rudvlf0413@naver.com>
2017-09-27 16:33:47 +0900

    update

--- master.py
+++ master.py
@@ -1,38 +1,61 @@
 # coding: utf-8
 
 from distributed import Client
-import redis
+from sklearn.preprocessing import normalize
+from worker import work
 import numpy as np
+import redis
 import pickle
-import time
-import random
-
 
 # master node에 redis db 실행해야함
 # master에서 dask-scheduler 실행
 # 각 worker pc에서는 dask-worker <마스터 ip>:8786
 
+root = 'fb15k/'
+data_files = ['train.txt', 'dev.txt', 'test.txt']
+
+entities = set()
+relations = set()
+
+for file in data_files:
+    with open(root+file, 'r') as f:
+        for line in f:
+            head, relation, tail = line[-1].split("\t")
+            entities.add(head)
+            entities.add(tail)
+            relations.add(relation)
+
+entities = sorted(entities)
+entity_id = {e: i for i, e in enumerate(entities)}
+relations = sorted(relations)
+relation_id = {r: i for i, r in enumerate(relations)}
 
 
-def randomSleep(i):
-    print(f"{i}th worker start")
-    time.sleep(random.randint(1, 5))
-    print(f"{i}th worker finished")
-    return i
+
+r = redis.StrictRedis(host='163.152.20.66', port=6379, db=0, password='davian!')
+
+r.mset({entity_id})
+r.mset(relation_id)
+r.set('entities', pickle.dumps(entities))
+r.set('relations', pickle.dumps(relations))
+
+entities_initialized = normalize(np.random.random((len(entities), 300)))
+relations_initialized = normalize(np.random.random((len(relations), 300)))
+
+r.mset({entity+'_v': pickle.dumps(entities_initialized[i]) for i, entity in enumerate(entities)})
+r.mset({relation+'_v': pickle.dumps(relations_initialized[i]) for i, relation in enumerate(relations)})
 
 
 client = Client('163.152.20.66:8786', asynchronous=True)
 
-results = []
-
 # 작업 배정
+results = []
 for i in range(10):
     # worker.py 호출
-    results.append(client.submit(randomSleep, i))
+    results.append(client.submit(work, i))
 
 print("aa")
 # max-min cut 실행
-
 
 # worker들 작업 끝나면 anchor 등 재분배
 for result in results:
--- worker.py
+++ worker.py
@@ -2,19 +2,42 @@
 
 from subprocess import Popen
 import redis
+import pickle
 import shlex
 
 
-def work():
+def work(worker_id):
     r = redis.StrictRedis(host='163.152.20.66', port=6379, db=0, password='davian!')
 
     # redis에서 embedding vector들 받아오기
+    entities = pickle.loads(r.get('entities'))
+    relations = pickle.loads(r.get('relations'))
 
-    vectors = r.mget([f'_{i}' for i in range(a.shape[0])])
-    pickle.loads(vectors[i])
+    entity_id = r.mget(entities)
+    entity_id = {entity: int(entity_id[i]) for i, entity in entities}
 
+    relation_id = r.mget(relations)
+    relation_id = {relation: int(relation_id[i]) for i, relation in relations}
+
+    entities_initialized = r.mget([entity+'_v' for entity in entities])
+    entities_initialized = [pickle.loads(v) for v in entities_initialized]
+
+    relations_initialzied = r.mget([relation+'_v' for relation in relations])
+    relations_initialized = [pickle.loads(v) for v in relations_initialized]
+    
     # 파일로 저장, c에서 불러와야!
+    with open(f"entity_vectors_{worker_id}.txt", 'w') as f:
+        for i, vector in enumerate(entities_initialized):
+            f.write(str(entities[i])+f"\t")
+            f.write(" ".join(vector)+'\n')
 
+    with open(f"relation_vectors_{worker_id}.txt", 'w') as f:
+        for i, relation in enumerate(relations_initialized):
+            f.write(str(relations[i])+f"\t")
+            f.write(" ".join(vector)+'\n')
+
+
+    """
     # c 프로그램 호출
     command = "bash test.sh"
     args = shlex.split(command)
@@ -26,3 +49,6 @@ def work():
 
     # redis에 해당 학습된 벡터들 업데이트하기
     r.mset({f'_{i}': pickle.dumps(v) for i, v in enumerate(a)})
+    """
+
+    return f"worker {worker_id}: finished"
