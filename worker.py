96a6e7d530492d9505685342273106c47d9769ff
강경필 <rudvlf0413@naver.com>
2017-09-27 20:07:03 +0900

    update

--- worker.py
+++ worker.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from subprocess import Popen
+from time import time
 import redis
 import pickle
 import shlex
@@ -9,6 +10,8 @@ import shlex
 r = redis.StrictRedis(host='163.152.20.66', port=6379, db=0, password='davian!')
 
 # redis에서 embedding vector들 받아오기
+print("redis...")
+t = time()
 entities = pickle.loads(r.get('entities'))
 relations = pickle.loads(r.get('relations'))
 
@@ -23,17 +26,21 @@ entities_initialized = [pickle.loads(v) for v in entities_initialized]
 
 relations_initialized = r.mget([relation+'_v' for relation in relations])
 relations_initialized = [pickle.loads(v) for v in relations_initialized]
+print(time()-t)
 
+print("save file...")
+print(time())
 # 파일로 저장, c에서 불러와야!
 with open(f"tmp/entity_vectors.txt", 'w') as f:
     for i, vector in enumerate(entities_initialized):
         f.write(str(entities[i])+f"\t")
-        f.write(" ".join(vector.tolist())+'\n')
+        f.write(" ".join([str(v) for v in vector])+'\n')
 
 with open(f"tmp/relation_vectors.txt", 'w') as f:
     for i, relation in enumerate(relations_initialized):
         f.write(str(relations[i])+f"\t")
-        f.write(" ".join(vector.tolist())+'\n')
+        f.write(" ".join([str(v) for v in vector])+'\n')
+print(time()-t)
 
 print("finished!")
 
@@ -44,10 +51,3 @@ command = "bash test.sh"
 args = shlex.split(command)
 proc = Popen(args)
 proc.wait()
-
-
-# embedding 학습 끝나면 c에서 업데이트한 것들 불러 오기
-
-# redis에 해당 학습된 벡터들 업데이트하기
-r.mset({f'_{i}': pickle.dumps(v) for i, v in enumerate(a)})
-"""
