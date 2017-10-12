45b8eb0610303ade5faa11a300ca02c276977c07
ahnjaewoo <ahnpersie@gmail.com>
2017-10-12 20:14:59 +0900

    added preprocessor.cpp

---
+++ MultiChannelEmbedding/DataModelPreprocessor.cpp
@@ -0,0 +1,13 @@
+#include "Import.hpp"
+#include "DetailedConfig.hpp"
+#include "ModelConfig.hpp"
+#include "DataModel.hpp"
+#include "Task.hpp"
+#include <omp.h>
+
+int main(int argc, char* argv[])
+{
+	DataModel* data_model =  new DataModel(FB15K, false);
+	cout << "Data Model preprocessing completed!" << endl;
+    return 0;
+}
