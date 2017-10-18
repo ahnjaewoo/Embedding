5693b7f429f577514a2b42eed97427363d498191
ahnjaewoo <ahnpersie@gmail.com>
2017-10-18 18:33:10 +0900

    corrected maxmin.py best issue

--- maxmin.py
+++ maxmin.py
@@ -22,7 +22,7 @@ anchor = set()
 non_anchor_edge_included_vertex = set()
 
 with open(data_file, 'r') as f:
-    for line in f:
+    for (idx,line) in enumerate(f):
         head, relation, tail = line[:-1].split("\t")
         entities.add(head)
         entities.add(tail)
@@ -33,6 +33,8 @@ with open(data_file, 'r') as f:
 
 entities_list = sorted(entities)
 entity2id = {e: i for i, e in enumerate(entities_list)}
+id2entity = {i: e for i, e in enumerate(entities_list)}
+entities_id = {entity2id[v] for v in entities}
 
 for (hd, tl) in entity_graph:
     edge_list.append((entity2id[hd], entity2id[tl]))
@@ -41,7 +43,7 @@ for (hd, tl) in entity_graph:
 try:
     with open(old_anchor_file, 'r') as f:
         for line in f:
-            old_anchor = set(entities_list[int(i)] for i in line[:-1].split(" "))
+            old_anchor = set(int(i) for i in line.split(" "))
 except:
     old_anchor = set()
 
@@ -49,24 +51,24 @@ except:
 for i in range(k):
     best = None
     best_score = 0
-    for vertex in entities.difference(anchor.union(old_anchor)):
+    for vertex in entities_id.difference(anchor.union(old_anchor)):
         # getting degree(v)
-        if len(connected_entity[vertex]) <= best_score:
+        if len(connected_entity[id2entity[vertex]]) <= best_score:
             continue
 
-        score = len(connected_entity[vertex].difference(anchor))
+        score = len(connected_entity[id2entity[vertex]].difference(anchor))
         if score > best_score:
-            best = entity2id[vertex]
+            best = vertex
             best_score = score
 
     if best == None:
         print("no vertex added to anchor")
     else:
-        print(best)
         anchor.add(best)
 
-with open(old_anchor_file, "w") as fwrite:
+with open(old_anchor_file, 'w') as fwrite:
     fwrite.write(" ".join([str(i) for i in anchor]))
+fwrite.close()
 
 # solve the min-cut partition problem of A~, finding A~ and edges
 non_anchor = entities.difference(anchor)
@@ -102,6 +104,7 @@ with open(output_file, "w") as fwrite:
     fwrite.write(" ".join([str(i) for i in anchor])+"\n")
     for nas in parts:
         fwrite.write(" ".join([str(i) for i in nas])+"\n")
+fwrite.close()
 
 print("Created anchor & non anchor sets by max-min cut algorithm successfully!")
 print(time()-t_)
