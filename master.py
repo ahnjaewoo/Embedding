2aa1241bebea9ff3c086fea34726564720896550
강경필 <rudvlf0413@naver.com>
2018-03-15 15:42:22 +0900

    fix bug - dask distributed asynchronous bug effecient socket connection arrange code

--- master.py
+++ master.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from distributed import Client
-from distributed.client import as_completed
+from distributed.diagnostics import progress
 from sklearn.preprocessing import normalize
 from subprocess import Popen
 from argparse import ArgumentParser
@@ -17,21 +17,35 @@ import struct
 import sys
 
 parser = ArgumentParser(description='Distributed Knowledge Graph Embedding')
-parser.add_argument('--num_worker', type=int, default=2, help='number of workers')
-parser.add_argument('--data_root', type=str, default='/fb15k', help='root directory of data(must include a name of dataset)')
-parser.add_argument('--niter', type=int, default=2, help='total number of masters iterations')
-parser.add_argument('--train_iter', type=int, default=10, help='total number of workers(actual) training iterations')
-parser.add_argument('--install', default=True, help='install libraries in each worker')
-parser.add_argument('--ndim', type=int, default=20, help='dimension of embeddings')
+parser.add_argument('--num_worker', type=int,
+                    default=2, help='number of workers')
+parser.add_argument('--data_root', type=str, default='/fb15k',
+                    help='root directory of data(must include a name of dataset)')
+parser.add_argument('--niter', type=int, default=2,
+                    help='total number of masters iterations')
+parser.add_argument('--train_iter', type=int, default=10,
+                    help='total number of workers(actual) training iterations')
+parser.add_argument('--install', default=True,
+                    help='install libraries in each worker')
+parser.add_argument('--ndim', type=int, default=20,
+                    help='dimension of embeddings')
 parser.add_argument('--lr', type=float, default=0.1, help='learning rate')
 parser.add_argument('--margin', type=int, default=2, help='margin')
-parser.add_argument('--anchor_num', type=int, default=5, help='number of anchor during entity training')
-parser.add_argument('--anchor_interval', type=int, default=6, help='number of epoch that anchors can rest as non-anchor')
-parser.add_argument('--root_dir', type=str, default="/home/rudvlf0413/distributedKGE/Embedding", help='project directory')
+parser.add_argument('--anchor_num', type=int, default=5,
+                    help='number of anchor during entity training')
+parser.add_argument('--anchor_interval', type=int, default=6,
+                    help='number of epoch that anchors can rest as non-anchor')
+parser.add_argument('--root_dir', type=str,
+                    default="/home/rudvlf0413/distributedKGE/Embedding", help='project directory')
 parser.add_argument('--temp_dir', type=str, default='', help='temp directory')
-parser.add_argument('--pypy_dir', type=str, default="/home/rudvlf0413/pypy/bin/pypy", help='pypy directory')
-parser.add_argument('--redis_ip', type=str, default='163.152.29.73', help='redis ip address')
-parser.add_argument('--scheduler_ip', type=str, default='163.152.29.73:8786', help='dask scheduler ip:port')
+parser.add_argument('--pypy_dir', type=str,
+                    default="/home/rudvlf0413/pypy/bin/pypy", help='pypy directory')
+parser.add_argument('--redis_ip', type=str,
+                    default='163.152.29.73', help='redis ip address')
+parser.add_argument('--scheduler_ip', type=str,
+                    default='163.152.29.73:8786', help='dask scheduler ip:port')
+parser.add_argument('--use_scheduler_config_file', default=False,
+                    help='wheter to use scheduler config file or use scheduler ip directly')
 args = parser.parse_args()
 
 install = args.install
@@ -41,16 +55,18 @@ if data_root[0] != '/':
     sys.exit(1)
 
 root_dir = args.root_dir
-logging.basicConfig(filename='%s/master.log' % root_dir, filemode='w', level=logging.DEBUG)
+logging.basicConfig(filename='%s/master.log' %
+                    root_dir, filemode='w', level=logging.DEBUG)
 logger = logging.getLogger()
 handler = logging.StreamHandler(stream=sys.stdout)
 logger.addHandler(handler)
+
 
 def handle_exception(exc_type, exc_value, exc_traceback):
     if issubclass(exc_type, KeyboardInterrupt):
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
         return
-    
+
     logger.error("exception", exc_info=(exc_type, exc_value, exc_traceback))
 
 sys.excepthook = handle_exception
@@ -66,8 +82,10 @@ if args.temp_dir == '':
 pypy_dir = args.pypy_dir
 redis_ip_address = args.redis_ip
 dask_ip_address = args.scheduler_ip
+use_scheduler_config_file = args.use_scheduler_config_file
 
-data_files = ['%s/train.txt' % data_root, '%s/dev.txt' % data_root, '%s/test.txt' % data_root]
+data_files = ['%s/train.txt' % data_root, '%s/dev.txt' %
+              data_root, '%s/test.txt' % data_root]
 num_worker = args.num_worker
 niter = args.niter
 train_iter = args.train_iter
@@ -77,6 +95,7 @@ margin = args.margin
 anchor_num = args.anchor_num
 anchor_interval = args.anchor_interval
 use_socket = True
+
 
 def data2id(data_root):
     data_root_split = [x.lower() for x in data_root.split('/')]
@@ -90,6 +109,7 @@ def data2id(data_root):
         print("[error] data root mismatch")
         sys.exit(1)
 
+
 data_root_id = data2id(data_root)
 
 # 여기서 전처리 C++ 프로그램 비동기 호출
@@ -97,7 +117,8 @@ master_start = time()
 t_ = time()
 print("Preprocessing start...")
 logger.warning("Preprocessing start...\n")
-proc = Popen(["%spreprocess.out" % preprocess_folder_dir, str(data_root_id)], cwd=preprocess_folder_dir)
+proc = Popen(["%spreprocess.out" % preprocess_folder_dir,
+              str(data_root_id)], cwd=preprocess_folder_dir)
 
 print("read files")
 logger.warning("read files\n")
@@ -146,7 +167,7 @@ for i, (relation, num) in enumerate(relation_each_num):
 print('# of relations per each partitions: [%s]' %
       " ".join([str(len(relation_list)) for relation_list, num in allocated_relation_worker]))
 logger.warning('# of relations per each partitions: [%s]\n' %
-      " ".join([str(len(relation_list)) for relation_list, num in allocated_relation_worker]))
+               " ".join([str(len(relation_list)) for relation_list, num in allocated_relation_worker]))
 
 sub_graphs = {}
 for c, (relation_list, num) in enumerate(allocated_relation_worker):
@@ -164,7 +185,6 @@ del relation_each_num
 del relation_triples
 del allocated_relation_worker
 del sub_graphs
-
 
 r.mset(entity2id)
 r.mset(relation2id)
@@ -215,8 +235,11 @@ def work(chunk_data, worker_id, cur_iter, n_dim, lr, margin, train_iter, data_ro
 
 #     return "%s finish saving file!" % worker_id
 
+if use_scheduler_config_file:
+    client = Client(scheduler_file=temp_folder_dir + '/scheduler.json', name='Embedding')
+else:
+    client = Client(dask_ip_address, name='Embedding')
 
-client = Client(dask_ip_address, asynchronous=True, name='Embedding')
 if install:
     client.run(install_libs)
 
@@ -249,12 +272,17 @@ if use_socket:
                   '0', str(anchor_num), str(anchor_interval), root_dir, data_root])
     print("popen maxmin.py complete - master.py")
     logger.warning("popen maxmin.py complete - master.py\n")
-    tt.sleep(3)
 
     maxmin_addr = '127.0.0.1'
     maxmin_port = 7847
-    maxmin_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    maxmin_sock.connect((maxmin_addr, maxmin_port))
+    while True:
+        try:
+            maxmin_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            maxmin_sock.connect((maxmin_addr, maxmin_port))
+            break
+        except TimeoutError:
+            tt.sleep(1)
+
     print("socket between master and maxmin connected - master.py")
     logger.warning("socket between master and maxmin connected - master.py\n")
 
@@ -266,7 +294,7 @@ if use_socket:
 
     # maxmin 의 결과를 소켓으로 받음
     anchor_len = struct.unpack('!i', maxmin_sock.recv(4))[0]
-    logger.warning(str(anchor_len)+'\n')
+    logger.warning(str(anchor_len) + '\n')
 
     for anchor_idx in range(anchor_len):
         anchors += str(struct.unpack('!i', maxmin_sock.recv(4))[0]) + " "
@@ -280,8 +308,7 @@ if use_socket:
             chunk += str(struct.unpack('!i', maxmin_sock.recv(4))[0]) + " "
         chunk = chunk[:-1]
         chunks.append(chunk)
-    # logger.warning(str(chunks)+'\n')
-
+    
 # max-min cut 실행, anchor 분배, 파일로 결과 전송
 else:
     proc = Popen([pypy_dir, '%s/maxmin.py' % root_dir, str(num_worker),
@@ -297,17 +324,12 @@ print("worker training iteration epoch: ", train_iter)
 logger.warning("worker training iteration epoch: {}".format(train_iter))
 for cur_iter in range(niter):
     logger.warning("%d iteration" % cur_iter)
-    
+
     t_ = time()
-    workers = list()
 
     # 작업 배정
-    for i in range(num_worker):
-        worker_id = 'worker_%d' % i
-        chunk_data = "{}\n{}".format(anchors, chunks[i])
-        workers.append(client.submit(work, chunk_data, worker_id,
-                                     cur_iter, n_dim, lr, margin, train_iter, data_root_id))
-    logger.warning("finished distributing jobs")
+    workers = client.map(work, [["{}\n{}".format(anchors, chunks[i]), 'worker_%d' %
+                                 i, cur_iter, n_dim, lr, margin, train_iter, data_root_id] for i in range(num_worker)])
 
     if cur_iter % 2 == 1:
         # entity partitioning: max-min cut 실행, anchor 등 재분배
@@ -336,9 +358,8 @@ for cur_iter in range(niter):
             anchor_len = struct.unpack('!i', maxmin_sock.recv(4))[0]
 
             for anchor_idx in range(anchor_len):
-                anchors+= str(struct.unpack('!i', maxmin_sock.recv(4))[0]) + " "
+                anchors += str(struct.unpack('!i', maxmin_sock.recv(4))[0]) + " "
             anchors = anchors[:-1]
-
 
             for part_idx in range(num_worker):
                 chunk = ""
@@ -353,7 +374,8 @@ for cur_iter in range(niter):
         # relation partitioning
         chunk_data = ''
 
-    for worker in as_completed(workers):
+    progress(workers)
+    for worker in workers:
         print(worker.result())
         logger.warning(worker.result())
 
@@ -369,11 +391,12 @@ entities = pickle.loads(r.get('entities'))
 relations = pickle.loads(r.get('relations'))
 entity_id = r.mget(entities)
 relation_id = r.mget(relations)
-entities_initialized = r.mget([entity+'_v' for entity in entities])
-relations_initialized = r.mget([relation+'_v' for relation in relations])
+entities_initialized = r.mget([entity + '_v' for entity in entities])
+relations_initialized = r.mget([relation + '_v' for relation in relations])
 
 entity_id = {entity: int(entity_id[i]) for i, entity in enumerate(entities)}
-relation_id = {relation: int(relation_id[i]) for i, relation in enumerate(relations)}
+relation_id = {relation: int(relation_id[i])
+               for i, relation in enumerate(relations)}
 
 entities_initialized = [pickle.loads(v) for v in entities_initialized]
 relations_initialized = [pickle.loads(v) for v in relations_initialized]
@@ -384,19 +407,23 @@ proc = Popen([
     cwd=preprocess_folder_dir)
 
 if use_socket:
-
     maxmin_sock.send(struct.pack('!i', 1))
     maxmin_sock.close()
-    
-    tt.sleep(2)
 
     test_addr = '0.0.0.0'
-    test_port = 7874 # 임의로 7874 로 포트를 정함
-    test_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    test_sock.connect((test_addr, test_port))
+    test_port = 7874  # 임의로 7874 로 포트를 정함
+
+    while True:
+        try:
+            test_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            test_sock.connect((test_addr, test_port))
+            break
+        except TimeoutError:
+            tt.sleep(1)
 
     test_sock.send(struct.pack('!i', 0))                        # 연산 요청 메시지
-    test_sock.send(struct.pack('!i', int(worker_id.split('_')[1])))           # int 임시 땜빵, 매우 큰 문제
+    # int 임시 땜빵, 매우 큰 문제
+    test_sock.send(struct.pack('!i', int(worker_id.split('_')[1])))
     test_sock.send(struct.pack('!i', int(cur_iter)))            # int
     test_sock.send(struct.pack('!i', int(n_dim)))       # int
     test_sock.send(struct.pack('d', float(lr)))      # double
@@ -450,9 +477,9 @@ if use_socket:
             test_sock.send(struct.pack('d', float(v)))
 
     del entities_initialized
-    del relations_initialized    
+    del relations_initialized
 
 proc.wait()
 
 print("Total elapsed time: %f" % (time() - master_start))
-logger.warning("Total elapsed time: %f\n" % (time() - master_start)) 
+logger.warning("Total elapsed time: %f\n" % (time() - master_start))
