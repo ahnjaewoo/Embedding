a5cf970a478c3a06595831849a118fd07a696863
Youngwoo Cho <cyw314@gmail.com>
2018-03-29 17:28:03 +0900

    revert

--- MultiChannelEmbedding/Embedding.cpp
+++ MultiChannelEmbedding/Embedding.cpp
@@ -46,7 +46,7 @@ int main(int argc, char* argv[])
 		int flag_iter;
 		int end_iter;
 		unsigned int len;
-		int embedding_sock[5], worker_sock;
+		int embedding_sock, worker_sock;
 		struct sockaddr_in embedding_addr;
 		struct sockaddr_in worker_addr;
 
@@ -65,44 +65,33 @@ int main(int argc, char* argv[])
 		//	printf("[error] setsocketopt in embedding.cpp\n");
 		//}
 
-		for(int temp = 0; temp < 5; temp++){
-
-			embedding_addr.sin_port = htons(49900 + worker_num * 5 + temp);
+		while (1){
 
 			// create socket and check it is valid
-			if ((embedding_sock[temp] = socket(PF_INET, SOCK_STREAM, 0)) < 0){
+			if ((embedding_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0){
 
 				printf("[error] create socket in embedding.cpp\n");
 				return -1;
 			}
 
-			if (bind(embedding_sock[temp], (struct sockaddr *)&embedding_addr, sizeof(embedding_addr)) < 0){
+			if (bind(embedding_sock, (struct sockaddr *)&embedding_addr, sizeof(embedding_addr)) < 0){
 
 				printf("[error] bind socket in embedding.cpp\n");
 				return -1;
 			}
 
-			if (listen(embedding_sock[temp], 1) < 0){
+			if (listen(embedding_sock, 1) < 0){
 
 				printf("[error] listen socket in embedding.cpp\n");
 				return -1;
 			}
 
-			printf("[Info] create socket %d in worker %d in embedding.cpp\n", temp, worker_num);
-		}
-
-		while (1){
-
 			len = sizeof(worker_addr);
 
-			if ((worker_sock = accept(embedding_sock[master_epoch % 5], (struct sockaddr *)&worker_addr, &len)) < 0){
+			if ((worker_sock = accept(embedding_sock, (struct sockaddr *)&worker_addr, &len)) < 0){
 
 				printf("[error] accept socket in embedding.cpp\n");
 				return -1;
-			}
-			else{
-
-				printf("[Info] accept socket in embedding.cpp\n");
 			}
 
 			if (recv(worker_sock, &flag_iter, sizeof(flag_iter), 0) < 0){
@@ -163,7 +152,7 @@ int main(int argc, char* argv[])
 			}
 
 			worker_num = ntohl(worker_num);
-			master_epoch = ntohl(master_epoch) + 1;
+			master_epoch = ntohl(master_epoch);
 			dim = ntohl(dim);
 			data_root_id = ntohl(data_root_id);
 
@@ -202,6 +191,8 @@ int main(int argc, char* argv[])
 
 			delete model;
 			close(worker_sock);
+
+			embedding_addr.sin_port = htons(49900 + worker_num * 5 + (master_epoch + 1) % 5);
 		}
 	}
 	else 
