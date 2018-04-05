dea6593baa708a782b1945880ad94b1ee0535c6d
강경필 <rudvlf0413@naver.com>
2018-04-05 19:33:50 +0900

    remove openbas options

--- preprocess/compile2.sh
+++ preprocess/compile2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-icc -std=c++11 -O3 -xHost -qopenmp -march=native preprocess.cpp -o preprocess.out -I /users/npark2/.local/usr/include/ -I /users/npark2/.local/usr/include/armadillo_bits -llapack -lopenblas -lboost_system -L /apps/pkg/openblas-0.2.18/rhel7_u2-x86_64-nehalem/gnu/lib -L /users/npark2/.local/usr/lib64 -larmadillo -lblas -I /users/npark2/.local/include/boost -lboost_serialization
+icc -std=c++11 -O3 -xHost -qopenmp -march=native preprocess.cpp -o preprocess.out -I /users/npark2/.local/usr/include/ -I /users/npark2/.local/usr/include/armadillo_bits -llapack -lboost_system -L /apps/pkg/openblas-0.2.18/rhel7_u2-x86_64-nehalem/gnu/lib -L /users/npark2/.local/usr/lib64 -larmadillo -lblas -I /users/npark2/.local/include/boost -lboost_serialization
