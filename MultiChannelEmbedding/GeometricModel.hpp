e75de6156f4dd0cab6a9d62dbcd0fd53fd81cfc7
Lactobacillus <cyw314@gmail.com>
2018-05-10 00:49:23 +0900

    aa

--- MultiChannelEmbedding/GeometricModel.hpp
+++ MultiChannelEmbedding/GeometricModel.hpp
@@ -733,6 +733,7 @@ public:
 
 						
 						// entity key 가 int 형식으로 주어짐
+						int entity_id;
 		                if (recv(fd, &entity_id, sizeof(int), 0) < 0){
 
 		                	printf("[error] recv entity_id in GeometricModel.hpp\n");
@@ -826,6 +827,7 @@ public:
 
 						
 						// relation key 가 int 형식으로 주어짐
+						int relation_id;
 		                if (recv(fd, &relation_id, sizeof(int), 0) < 0){
 
 		                	printf("[error] recv relation_id in GeometricModel.hpp\n");
