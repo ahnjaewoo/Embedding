b4382e67062a193aa58718ead2705c6734ee6522
Lactobacillus <cyw314@gmail.com>
2018-05-21 23:16:24 +0900

    small

--- MultiChannelEmbedding/Model.hpp
+++ MultiChannelEmbedding/Model.hpp
@@ -253,8 +253,12 @@ public:
 			//	<<lreal_hit/lreal_total;
 		}
 
+		printf("[Info] Model.hpp > true = %d\n", data_model.data_test_true.size());
+		printf("[Info] Model.hpp > false = %d\n", data_model.data_test_false.size());
 		printf("[Info] Model.hpp > true + false = %d\n", data_model.data_test_true.size() + data_model.data_test_false.size());
 		printf("[Info] Model.hpp > real_hit = %lf\n", real_hit);
+		fprintf(fs_log, "[Info] Model.hpp > true = %d\n", data_model.data_test_true.size());
+		fprintf(fs_log, "[Info] Model.hpp > false = %d\n", data_model.data_test_false.size());
 		fprintf(fs_log, "[Info] Model.hpp > true + false = %d\n", data_model.data_test_true.size() + data_model.data_test_false.size());
 		fprintf(fs_log, "[Info] Model.hpp > real_hit = %lf\n", real_hit);
 
