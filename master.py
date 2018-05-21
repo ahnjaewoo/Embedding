e5a33860763c52eb2f59f41ceaf3fef87c72fa69
Youngwoo Cho <cyw314@gmail.com>
2018-05-21 20:21:50 +0900

    log dir

--- master.py
+++ master.py
@@ -272,7 +272,7 @@ def work(chunk_data, worker_id, cur_iter, n_dim, lr, margin, train_iter, data_ro
     # dask 에 submit 하는 함수에는 logger.warning 을 사용하면 안됨
     socket_port = 50000 + 5 * int(worker_id.split('_')[1]) + (cur_iter % 5)
     print('[info] master.py > work function called, cur_iter = ' + str(cur_iter) + ', port = ' + str(socket_port))
-    log_dir = os.path.join(root_dir, 'embedding_log_' + worker_id + '_iter_' + str(cur_iter) + '.txt')
+    log_dir = os.path.join(root_dir, 'logs/embedding_log_' + worker_id + '_iter_' + str(cur_iter) + '.txt')
 
     embedding_proc = Popen([train_code_dir, 
                                     worker_id,
@@ -574,7 +574,7 @@ maxmin_sock.close()
 ###############################################################################
 
 worker_id = 'tester_0'
-log_dir = os.path.join(root_dir, 'test_log_' + worker_id + '.txt')
+log_dir = os.path.join(root_dir, 'logs/test_log_' + worker_id + '.txt')
 proc = Popen([test_code_dir,
             worker_id,
             str(cur_iter),
