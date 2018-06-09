507f4d27b4d2cd56d5d506348eb251f7a11f849d
ahnjaewoo <ahnpersie@gmail.com>
2018-06-09 18:32:52 +0900

    changed master.py's test code

--- master.py
+++ master.py
@@ -374,6 +374,7 @@ while True:
     try:
     
         maxmin_sock.connect(('127.0.0.1', 7847))
+        printt('[info] master > maxmin connection succeed')
         break
     
     except Exception as e:
@@ -548,7 +549,7 @@ worker_id = 'worker_0'
 log_dir = os.path.join(args.root_dir, 'logs/test_log.txt')
 proc = Popen([test_code_dir,
             worker_id,
-            str(cur_iter),
+            '0',
             str(n_dim),
             str(lr),
             str(margin),
@@ -590,76 +591,44 @@ chunk_entity = list()
 checksum = 0
 success = 0
 
-# 이거 이상한데??
-if int(cur_iter) % 2 == 0:
-    # entity 전송 - DataModel 생성자
-    chunk_anchor = list()
-    chunk_entity = list()
+# entity 전송 - DataModel 생성자
+chunk_anchor = list()
+chunk_entity = list()
 
-    if len(chunk_anchor) is 1 and chunk_anchor[0] is '':
+if len(chunk_anchor) is 1 and chunk_anchor[0] is '':
     
-        chunk_anchor = []
+    chunk_anchor = []
 
-    while success != 1:
+while success != 1:
 
-        test_sock.send(struct.pack('!i', len(chunk_anchor)))
+    test_sock.send(struct.pack('!i', len(chunk_anchor)))
 
-        for iter_anchor in chunk_anchor:
+    for iter_anchor in chunk_anchor:
         
-            test_sock.send(struct.pack('!i', int(iter_anchor)))
+        test_sock.send(struct.pack('!i', int(iter_anchor)))
 
-        test_sock.send(struct.pack('!i', len(chunk_entity)))
+    test_sock.send(struct.pack('!i', len(chunk_entity)))
 
-        for iter_entity in chunk_entity:
+    for iter_entity in chunk_entity:
         
-            test_sock.send(struct.pack('!i', int(iter_entity)))
+        test_sock.send(struct.pack('!i', int(iter_entity)))
 
-        checksum = struct.unpack('!i', sockRecv(test_sock, 4))[0]
+    checksum = struct.unpack('!i', sockRecv(test_sock, 4))[0]
 
-        if checksum == 1234:
+    if checksum == 1234:
 
-            # printt('[info] master > phase 1 finished (for test)')
-            success = 1
+        # printt('[info] master > phase 1 finished (for test)')
+        success = 1
 
-        elif checksum == 9876:
+    elif checksum == 9876:
 
-            printt('[error] master > retry phase 1 (for test)')
-            success = 0
+        printt('[error] master > retry phase 1 (for test)')
+        success = 0
 
-        else:
+    else:
 
-            printt('[error] master > unknown error in phase 1 (for test)')
-            success = 0
-
-else:
-    # relation 전송 - DataModel 생성자
-    sub_graphs = pickle.loads(r.get('sub_graph_{}'.format('worker_0')))
-    test_sock.send(struct.pack('!i', len(sub_graphs)))
-
-    while success != 1:
-
-        for (head_id, relation_id, tail_id) in sub_graphs:
-
-            test_sock.send(struct.pack('!i', int(head_id)))
-            test_sock.send(struct.pack('!i', int(relation_id)))
-            test_sock.send(struct.pack('!i', int(tail_id)))
-
-        checksum = struct.unpack('!i', sockRecv(test_sock, 4))[0]
-
-        if checksum == 1234:
-
-            # printt('[info] master > phase 1 finished (for test)')
-            success = 1
-
-        elif checksum == 9876:
-
-            printt('[error] master > retry phase 1 (for test)')
-            success = 0
-
-        else:
-
-            printt('[error] master > unknown error in phase 1 (for test)')
-            success = 0
+        printt('[error] master > unknown error in phase 1 (for test)')
+        success = 0
 
 printt('[info] master > chunk or relation sent to DataModel (for test)')
 
@@ -764,6 +733,8 @@ sockSaveTime = list()
 redisTime = list()
 workerTotalTime = list()
 
+print("worker logs")
+print(workerLogs)
 for key in workerLogs:
 
     redisConnTime.append(workerLogs[key]["\n== redis_conn_time = {}\n"])
