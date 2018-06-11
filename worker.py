bcde9b350d0e5d8032b803649967c2106a40a4d6
강경필 <rudvlf0413@naver.com>
2018-06-11 21:52:12 +0900

    remove loggerOn variable

--- worker.py
+++ worker.py
@@ -29,17 +29,11 @@ if debugging == 'yes':
     logger = logging.getLogger()
     handler = logging.StreamHandler(stream=sys.stdout)
     logger.addHandler(handler)
-    loggerOn = True
 
     def printt(str):
 
-        global loggerOn
-
         print(str)
-
-        if loggerOn:
-
-            logger.warning(str + '\n')
+        logger.warning(str + '\n')
 
     def handle_exception(exc_type, exc_value, exc_traceback):
 
