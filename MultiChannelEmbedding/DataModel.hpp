d981be938f8b35943bb8bf81fcbd6634b23b75f5
Lactobacillus <cyw314@gmail.com>
2018-06-16 17:42:08 +0900

    test

--- MultiChannelEmbedding/DataModel.hpp
+++ MultiChannelEmbedding/DataModel.hpp
@@ -270,7 +270,7 @@ public:
                         }
 
                         // 원소 하나씩 받음
-                        
+                        /*
                         for (int idx = 0; idx < ntohl(anchor_num); idx++) {
 
                             if (recv(fd, &temp_value, sizeof(temp_value), MSG_WAITALL) < 0){
@@ -290,11 +290,11 @@ public:
                             check_anchor[temp_value] = true;
                             check_parts[temp_value] = true;
                         }
-    
+                        */
                         //.....................
 
                         // 원소 한 번에 받음
-                        /*
+                        
                         int * anchor_buff = (int *)calloc(ntohl(anchor_num) + 1, sizeof(int));
                         if (recv(fd, anchor_buff, ntohl(anchor_num) * sizeof(int), MSG_WAITALL) < 0){
 
@@ -317,7 +317,7 @@ public:
                         }
                         
                         free(anchor_buff);
-                        */
+                        
                         //.....................
 
                         if (recv(fd, &entity_num, sizeof(entity_num), MSG_WAITALL) < 0){
@@ -333,7 +333,7 @@ public:
                         }
 
                         // 원소 하나씩 받음
-                        
+                        /*
                         for (int idx = 0; idx < ntohl(entity_num); idx++) {
 
                             if (recv(fd, &temp_value, sizeof(temp_value), MSG_WAITALL) < 0){
@@ -352,11 +352,11 @@ public:
                             set_entity_parts.insert(temp_value);
                             check_parts[temp_value] = true;
                         }
-                        
+                        */
                         //.....................
 
                         // 원소 한 번에 받음
-                        /*
+                        
                         int * entity_buff = (int *)calloc(ntohl(entity_num) + 1, sizeof(int));
                         if (recv(fd, entity_buff, ntohl(entity_num) * sizeof(int), MSG_WAITALL) < 0){
 
@@ -378,7 +378,7 @@ public:
                         }
 
                         free(entity_buff);
-                        */
+                        
                         //.....................
 
                         for (auto i = data_train.begin(); i != data_train.end(); ++i) {
