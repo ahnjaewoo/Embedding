8c45bdf99e7eb3168b179ea51076338dedffd396
Youngwoo Cho <cyw314@gmail.com>
2018-07-03 20:42:59 +0900

    test

--- maxmin.py
+++ maxmin.py
@@ -29,7 +29,7 @@ if debugging == 'yes':
     logger = logging.getLogger()
     handler = logging.StreamHandler(stream=sys.stdout)
     logger.addHandler(handler)
-    loggerOn = False
+    loggerOn = True
 
     def printt(str):
 
