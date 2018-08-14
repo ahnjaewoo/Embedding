ae04024f3a00a375b3e7d450782d30ed9053a7bc
Youngwoo Cho <cyw314@gmail.com>
2018-08-14 14:46:46 +0900

    g

--- MultiChannelEmbedding/GeometricModel.hpp
+++ MultiChannelEmbedding/GeometricModel.hpp
@@ -1827,7 +1827,7 @@ public:
 			//cout << "embedding_entity[triplet.first.first]  " << embedding_entity[triplet.first.first] << endl;
 			//cout << "embedding_clusters[triplet.second][c]  " << embedding_clusters[triplet.second][c] << endl;
 			//cout << "embedding_entity[triplet.first.second]  " << embedding_entity[triplet.first.second] << endl;
-			//cout << "error_c  " << error_c << endl;
+			cout << "error_c  " << error_c << endl;
 			//cout << "weights_clusters[triplet.second][c]  " << weights_clusters[triplet.second][c] << endl;
 		}
 
