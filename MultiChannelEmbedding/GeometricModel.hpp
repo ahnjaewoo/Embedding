f40d1bcc381a51ba6afb29c80ecf069126f91870
Youngwoo Cho <cyw314@gmail.com>
2018-08-14 16:05:30 +0900

    gg

--- MultiChannelEmbedding/GeometricModel.hpp
+++ MultiChannelEmbedding/GeometricModel.hpp
@@ -2486,7 +2486,7 @@ public:
 							// n_cluster 를 생각하지 않고 그냥 21 개의 값을 전송
 							weights_clusters[relation_id](j) = (float) vector_buff[j];
 							cout << "vector_buff[j]  " << vector_buff[j] << endl;
-							cout << "weights_clusters[relation_id](j)  " << weights_clusters[relation_id](j) << endl;
+							cout << "weights_clusters[relation_id](j)  " << (float)weights_clusters[relation_id](j) << endl;
 						}
 
 						free(vector_buff);
