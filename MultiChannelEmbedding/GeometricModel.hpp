ba297d1c7b8ee855da2c3bf39f9da8e8e686258f
강경필 <rudvlf0413@naver.com>
2018-08-20 23:10:32 +0900

    .

--- MultiChannelEmbedding/GeometricModel.hpp
+++ MultiChannelEmbedding/GeometricModel.hpp
@@ -2169,6 +2169,8 @@ public:
 					if (data_model.set_relation_parts.find(i) != data_model.set_relation_parts.end()){
 
 						vector_buff[idx++] = htonl(size_clusters[i]);
+
+						idx++;
 					}
 				}
 
