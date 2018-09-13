0b813dfd49cdd936359766b54b0b7c702d959e7b
Youngwoo Cho <cyw314@gmail.com>
2018-09-13 16:47:07 +0900

    gg

--- master.py
+++ master.py
@@ -142,7 +142,13 @@ for file in data_files:
 
         for line in f:
 
-            head, relation, tail = line[:-1].split("\t")
+            try:
+                
+                head, relation, tail = line[:-1].split("\t")
+
+            except:
+
+                printt(line[:-1])
 
             if head not in entity2id:
 
