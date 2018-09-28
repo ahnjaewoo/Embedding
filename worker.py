fcf3709add7c75dc08b5f4925a05b449a662e9d8
Youngwoo Cho <cyw314@gmail.com>
2018-09-28 16:44:47 +0900

    gggg bbbbbb

--- worker.py
+++ worker.py
@@ -444,8 +444,9 @@ try:
                 # 원소를 한 번에 받음
                 count_entity = unpack('!i', sockRecv(embedding_sock, 4))[0]
                 
-                embeddingTime = default_timer() - timeNow # 순수한 embedding 시간을 측정하기 위해서 여기 위치, cpp 가 send 하면 embedding 이 끝난 것
-                
+                embeddingTime = default_timer() - timeNow # 순수한 embedding 시간을 측정하기 위해서 여기 위치, cpp 가 send 하면 embedding 이 끝난 것                
+                fsLog.write('line 448 - embeddingTime : ' + str(embeddingTime) + '\n')
+
                 entity_id_list = unpack('!' + 'i' * count_entity, sockRecv(embedding_sock, count_entity * 4))
                 entity_vector_list = unpack(precision_string * count_entity * embedding_dim,
                     sockRecv(embedding_sock, precision_byte * embedding_dim * count_entity))
@@ -621,8 +622,8 @@ try:
         redisTime += default_timer() - timeNow
 
     fsLog.write('[info] worker > phase 3 finished\n')
-    fsLog.write('line 626 - sockLoadTime : ' + str(sockSaveTime) + '\n')
-    fsLog.write('line 627 - redisTime (cumulative) : ' + str(redisTime) + '\n')
+    fsLog.write('line 625 - sockLoadTime : ' + str(sockSaveTime) + '\n')
+    fsLog.write('line 626 - redisTime (cumulative) : ' + str(redisTime) + '\n')
 
 except Exception as e:
 
