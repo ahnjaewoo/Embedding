f5ef9139e650b1a6e48a7a75c8765eaf40f9d4c7
Lactobacillus <cyw314@gmail.com>
2018-10-14 20:35:17 +0900

    asdf

--- MultiChannelEmbedding/Embedding.cpp
+++ MultiChannelEmbedding/Embedding.cpp
@@ -185,18 +185,18 @@ int main(int argc, char* argv[]){
 
 	gettimeofday(&after, NULL);
 	run_time = after.tv_sec + after.tv_usec/1000000.0 - before.tv_sec - before.tv_usec/1000000.0;
-	//cout << "embedding > model->run end, training time : " << run_time << "seconds" << endl;
-	fprintf(fs_log, "[info] embedding > testing time : %lf seconds\n", run_time);
+	cout << "embedding > model->run end, training time : " << run_time << "seconds" << endl;
+	//fprintf(fs_log, "[info] embedding > testing time : %lf seconds\n", run_time);
 	
 	model->save(to_string(worker_num), fs_log);
-	//cout << "embedding > model->save end" << endl;
+	cout << "embedding > model->save end" << endl;
 	//fprintf(fs_log, "embedding > model->save end\n");
 
 	if (recv(worker_sock, &flag, sizeof(flag), MSG_WAITALL) < 0){
 		cout << "error 196" << endl;
 	}
-
-	if (ntohl(flag == 1234)){
+	cout << "flag = " << ntohl(flag) << endl;
+	if (ntohl(flag) == 1234){
 		send(worker_sock, &run_time, sizeof(run_time), 0);
 	}
 
