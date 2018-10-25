62edc5fc4e6992bc740ebbbb545a224e381ce181
Lactobacillus <cyw314@gmail.com>
2018-10-25 19:02:55 +0900

    adsf

--- master.py
+++ master.py
@@ -185,6 +185,8 @@ relation_each_num = [(k, len(v)) for k, v in relation_triples.items()]
 relation_each_num = sorted(relation_each_num, key=lambda x: x[1], reverse=True)
 allocated_relation_worker = [[[], 0] for i in range(num_worker)]
 
+printt('[info] master > relation_each_num : [%s]' % relation_each_num)
+
 for i, (relation, num) in enumerate(relation_each_num):
 
     allocated_relation_worker = sorted(allocated_relation_worker, key=lambda x: x[1])
